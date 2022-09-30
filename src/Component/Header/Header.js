05bf33536c2da8d8364a8e82b7d24ff99cc7837c
sarkerjahiduzzaman <78559065+sarkerjahiduzzaman@users.noreply.github.com>
2022-09-30 20:27:36 +0600

    header code is added

---
+++ src/Component/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <nav className='header'>
+            <h2>Ultra-Active-Motion</h2>
+            <p></p>
+        </nav>
+    );
+};
+
+export default Header;
+
---
+++ src/Component/fakeDb/Fakedb.js
@@ -0,0 +1,4 @@
+const addToDb=(id)=>{
+    
+}
+export {addToDb}
